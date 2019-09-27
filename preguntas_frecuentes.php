6fdff05a8e7e445e0c9fc356180c4855f3dfc532
Matias <matiasml@live.com.ar>
2019-09-26 22:53:15 -0300

    Preguntas Frecuentes versión 2

--- preguntas_frecuentes.php
+++ preguntas_frecuentes.php
@@ -3,15 +3,290 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta name="author" content="Bootstrap-ecommerce by Vosidiy">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <!-- jQuery -->
+    <script src="js/jquery-2.0.0.min.js" type="text/javascript"></script>
+
+    <!-- Bootstrap4 files-->
+    <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+
+    <!-- Font awesome 5 -->
+    <link href="fonts/fontawesome/css/fontawesome-all.min.css" type="text/css" rel="stylesheet">
+
+    <!-- plugin: owl carousel  -->
+    <link href="plugins/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="plugins/owlcarousel/assets/owl.theme.default.css" rel="stylesheet">
+    <script src="plugins/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- custom style -->
+    <link href="css/ui.css" rel="stylesheet" type="text/css"/>
+    <link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+
+    <!-- custom javascript -->
+    <script src="js/script.js" type="text/javascript"></script>
+
+    <script type="text/javascript">
+    /// some script
+
+    // jquery ready start
+    $(document).ready(function() {
+    	// jQuery code
+
+    });
+    // jquery end
+    </script>
+
     <title>SportLife</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
   </head>
-  <body>
-    <h1>Preguntas frecuentes</h1>
+  <body class="container">
+    <header class="section-header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container">
+            <a class="navbar-brand" href="#"><img class="logo" src="images/logos/logo-alibaba.png" alt="alibaba style e-commerce html template file" title="alibaba e-commerce html css theme"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTop" aria-controls="navbarTop" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTop">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">   Sourcing </a>
+                    <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Top  Suppliers</a></li>
+                <li><a class="dropdown-item" href="#">Suppliers by Regions </a></li>
+                <li><a class="dropdown-item" href="#">Online Retailer  </a></li>
+                    </ul>
+                </li>
+            <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">   Services </a>
+                    <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Trade Assurance </a></li>
+                <li><a class="dropdown-item" href="#">Arabic</a></li>
+                <li><a class="dropdown-item" href="#">Logistics Service </a></li>
+                <li><a class="dropdown-item" href="#">Membership Services</a></li>
+                    </ul>
+                </li>
+            <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">   Community </a>
+                    <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Help Center</a></li>
+                <li><a class="dropdown-item" href="#">Submit a Dispute </a></li>
+                <li><a class="dropdown-item" href="#">For Suppliers </a></li>
+                    </ul>
+                </li>
+              </ul>
+              <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link" > Multi Request </a></li>
+            <li class="nav-item"><a href="http://bootstrap-ecommerce.com/" class="nav-link" > Download  </a></li>
+
+              </ul> <!-- navbar-nav.// -->
+            </div> <!-- collapse.// -->
+          </div>
+        </nav>
+
+        <section class="header-main shadow-sm">
+          <div class="container">
+        <div class="row-sm align-items-center">
+          <div class="col-lg-4-24 col-sm-3">
+          <div class="category-wrap dropdown py-1">
+            <button type="button" class="btn btn-light  dropdown-toggle" data-toggle="dropdown" ><i class="fa fa-bars"></i> Categories</button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Machinery / Mechanical Parts / Tools </a>
+              <a class="dropdown-item" href="#">Consumer Electronics / Home Appliances </a>
+              <a class="dropdown-item" href="#">Auto / Transportation</a>
+              <a class="dropdown-item" href="#">Apparel / Textiles / Timepieces </a>
+              <a class="dropdown-item" href="#">Home & Garden / Construction / Lights </a>
+              <a class="dropdown-item" href="#">Beauty & Personal Care / Health </a>
+            </div>
+          </div>
+          </div>
+          <div class="col-lg-11-24 col-sm-8">
+              <form action="#" class="py-1">
+                <div class="input-group w-100">
+                  <select class="custom-select"  name="category_name">
+                    <option value="">All type</option>
+                    <option value="">Special</option>
+                    <option value="">Only best</option>
+                    <option value="">Latest</option>
+                  </select>
+                    <input type="text" class="form-control" style="width:50%;" placeholder="Search">
+                    <div class="input-group-append">
+                      <button class="btn btn-warning" type="submit">
+                        <i class="fa fa-search"></i> Search
+                      </button>
+                    </div>
+                  </div>
+              </form> <!-- search-wrap .end// -->
+          </div> <!-- col.// -->
+          <div class="col-lg-9-24 col-sm-12">
+            <div class="widgets-wrap float-right row no-gutters py-1">
+              <div class="col-auto">
+              <div class="widget-header dropdown">
+                <a href="#" data-toggle="dropdown" data-offset="20,10">
+                  <div class="icontext">
+                    <div class="icon-wrap"><i class="text-warning icon-sm fa fa-user"></i></div>
+                    <div class="text-wrap text-dark">
+                      Sign in <br>
+                      My account <i class="fa fa-caret-down"></i>
+                    </div>
+                  </div>
+                </a>
+                <div class="dropdown-menu">
+                  <form class="px-4 py-3">
+                    <div class="form-group">
+                      <label>Email address</label>
+                      <input type="email" class="form-control" placeholder="email@example.com">
+                    </div>
+                    <div class="form-group">
+                      <label>Password</label>
+                      <input type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    </form>
+                    <hr class="dropdown-divider">
+                    <a class="dropdown-item" href="#">Have account? Sign up</a>
+                    <a class="dropdown-item" href="#">Forgot password?</a>
+                </div> <!--  dropdown-menu .// -->
+              </div>  <!-- widget-header .// -->
+              </div> <!-- col.// -->
+              <div class="col-auto">
+                <a href="#" class="widget-header">
+                  <div class="icontext">
+                    <div class="icon-wrap"><i class="text-warning icon-sm fa fa-shopping-cart"></i></div>
+                    <div class="text-wrap text-dark">
+                      Order <br> Protection
+                    </div>
+                  </div>
+                </a>
+              </div> <!-- col.// -->
+              <div class="col-auto">
+                <a href="#" class="widget-header">
+                  <div class="icontext">
+                    <div class="icon-wrap"><i class="text-warning icon-sm  fa fa-heart"></i></div>
+                    <div class="text-wrap text-dark">
+                      <span class="small round badge badge-secondary">0</span>
+                      <div>Favorites</div>
+                    </div>
+                  </div>
+                </a>
+              </div> <!-- col.// -->
+            </div> <!-- widgets-wrap.// row.// -->
+          </div> <!-- col.// -->
+        </div> <!-- row.// -->
+          </div> <!-- container.// -->
+        </section> <!-- header-main .// -->
+    </header> <!-- section-header.// -->
+
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">SportLife</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Busca tus zapatillas y más... " aria-label="...">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        </form>
+      </div>
+    </nav>
+ -->
+    <section>
+      <br>
+      <h1 style="font-size: 36px">Preguntas frecuentes</h1>
+        <br>
+        <h2 class="text-body" style="font-size: 18px">¿Cuál es el horario de Atención al Cliente?</h2>
+          <p style="font-size: 14px">Nuestra Central de Atención al cliente funciona de lunes a sábados de 9:00hs a 17:00hs.</p>
+          <br>
+        <h2 class="text-body" style="font-size: 18px">¿Cuál es el plazo de entrega de mi pedido?</h2>
+          <p style="font-size: 14px">El plazo de entrega es de 1 a 2 días para la zona de CABA y entre 2 a 3 días en el Gran Buenos Aires.</p>
+          <br>
+        <h2 class="text-body" style="font-size: 18px">Si no hay nadie para recibir el pedido, ¿qué sucede?</h2>
+          <p style="font-size: 14px">Serán realizados 2 intentos de entrega. Después de los 2 intentos, si la entrega continua sin ser efectiva, el pedido permanecerá en la sucursal del correo encargado del envío durante 5 días hábiles para su retiro. Pasado este plazo el pedido retornará a nuestro centro de distribución. </p>
+          <br>
+        <h2 class="text-body" style="font-size: 18px">Todavía no recibí mi pedido,¿qué hago?</h2>
+          <p style="font-size: 14px">Deberá contactarse con sac@sportlife.com.ar</p>
+        <h2 class="text-body" style="font-size: 18px"></h2>
+    </section>
+
+    <footer class="section-footer bg-secondary">
+    	<div class="container">
+    		<section class="footer-top padding-top">
+    			<div class="row">
+    				<aside class="col-sm-3 col-md-3 white">
+    				 <h5 class="title">Atención al cliente</h5>
+    					<ul class="list-unstyled">
+    						<li> <a href="#">Centro de ayuda</a></li>
+    						<li> <a href="#">Devolución de dinero</a></li>
+    						<li> <a href="#">Términos y políticas</a></li>
+    					</ul>
+    				</aside>
+    				<aside class="col-sm-3  col-md-3 white">
+    					<h5 class="title">Mi cuenta</h5>
+    					<ul class="list-unstyled">
+    						<li> <a href="#"> Usuario </a></li>
+    						<li> <a href="#"> Configuración de mi cuenta </a></li>
+    						<li> <a href="#"> Mis compras </a></li>
+    					</ul>
+    				</aside>
+    				<aside class="col-sm-3  col-md-3 white">
+    					<h5 class="title">Sobre nosotros</h5>
+    					<ul class="list-unstyled">
+    						<li> <a href="#"> Historia </a></li>
+    						<li> <a href="#"> Quienes somos  </a></li>
+    						<li> <a href="#"> Trabajá con nosotros </a></li>
+    					</ul>
+    				</aside>
+    				<aside class="col-sm-3">
+    					<article class="white">
+    						<h5 class="title">Contacto</h5>
+    						<p>
+    							<strong>Telefono: </strong> +123456789
+                  <br>
+    						  <strong>Fax:</strong> +123456789
+                  <br>
+                  <strong>Mail:</strong> info@sportlife.com.ar
+    						</p>
+
+    						 <div class="btn-group white">
+    						    <a class="btn btn-facebook" title="Facebook" target="_blank" href="#"><i class="fab fa-facebook-f  fa-fw"></i></a>
+    						    <a class="btn btn-instagram" title="Instagram" target="_blank" href="#"><i class="fab fa-instagram  fa-fw"></i></a>
+    						    <a class="btn btn-youtube" title="Youtube" target="_blank" href="#"><i class="fab fa-youtube  fa-fw"></i></a>
+    						    <a class="btn btn-twitter" title="Twitter" target="_blank" href="#"><i class="fab fa-twitter  fa-fw"></i></a>
+    						  </div>
+    					   </article>
+    				    </aside>
+    			   </div> <!-- row.// -->
+    			   <br>
+    		 </section>
+    	</div><!-- //container -->
+    </footer>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
