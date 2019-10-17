154cd3b5d8a88b0884e83859d27b07b3a6a493d6
Matias <matiasml@live.com.ar>
2019-10-17 19:28:02 -0300

    Sprint 2 Final

--- contacto.php
+++ contacto.php
@@ -8,10 +8,6 @@ require_once 'navbar.php';
 
  ?>
 
-
-
-
-
 <!DOCTYPE html>
 <html lang="en" >
 
@@ -20,14 +16,17 @@ require_once 'navbar.php';
   <!-- ========================= MAIN ========================= -->
 
   <div class="container mt-5" class="nl-blurred" align="center">
-    <h1>Formulario de contacto</h1>
+    <h1 style="font-size: 40px">Formulario de contacto</h1>
     <p>Rellenalo con tu información y te contactaremos</p>
   <div class="main clearfix">
-    <form id="nl-form" class="nl-form">
-      Hola<img src="https://media.tenor.com/images/3b388fe03da271d2674faf85eb7c3fcd/tenor.gif" width="50px">, me llamo <input type="text" value="" placeholder="Nombre y apellido" data-subline="Ejemplo: <em>Escribi tu nombre y tu apellido</em>"/>
+    <form id="nl-form">
+      <p>Hola</p>
+      <img src="https://media.tenor.com/images/3b388fe03da271d2674faf85eb7c3fcd/tenor.gif" width="50px">
+       <p>me llamo</p>
+       <input type="text" value="" placeholder="Nombre y apellido" data-subline="Ejemplo: <em>Escribi tu nombre y tu apellido</em>"/>
       <br> y mi correo es
       <input type="text" value="" placeholder="@email.com" data-subline="Ejemplo: <em>Tu correo electronico</em>"/>
-      <br />quiero
+      quiero
       <select>
         <option value="1" selected>más información</option>
          <option value="1">hacer una sugerencia</option>
@@ -39,7 +38,7 @@ require_once 'navbar.php';
 
       <textarea name="text" id="text" cols="30" rows="3" placeholder="mi mensaje"></textarea>
       <br>
-        <button class="nl-submit" type="submit">Enviar</button>
+        <button class="btn btn-primary" type="submit">Enviar</button>
       </div>
       <div class="nl-overlay"></div>
     </form>
@@ -76,32 +75,7 @@ require_once 'navbar.php';
     </section>
     <!-- ========================= LINKS END.// ========================= -->
 
-    <!-- ========================= SUSCRIBETE ========================= -->
-    <section class="section-subscribe bg-secondary padding-y-lg">
-    <div class="container">
-
-    <p class="pb-2 text-center white">Suscríbete para obtener ofertas</p>
-
-    <div class="row justify-content-md-center">
-      <div class="col-lg-4 col-sm-6">
-    <form class="row-sm form-noborder">
-        <div class="col-8">
-        <input class="form-control" placeholder="Tu email" type="email">
-        </div> <!-- col.// -->
-        <div class="col-4">
-        <button type="submit" class="btn btn-block btn-dark suscribeteBoton"> <i class="fa fa-envelope"></i> Suscribirse </button>
-        </div> <!-- col.// -->
-    </form>
-    <small class="form-text text-white-50">Nunca compartiremos su dirección de correo electrónico con un tercero. </small>
-      </div> <!-- col-md-6.// -->
-    </div>
-
-
-    </div> <!-- container // -->
-    </section>
-    <!-- ========================= SUSCRIBETE END.//========================= -->
-
-<?php  require_once 'navbar.php'; ?>
+<?php  require_once 'footer.php'; ?>
 
 <!-- partial -->
   <script src='https://code.jquery.com/jquery-3.1.0.min.js'></script>
