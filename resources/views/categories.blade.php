0c980e25058e2283009d7d5d1738af8b074265aa
Matias <matiasml@live.com.ar>
2019-12-17 18:56:07 -0300

    Version 17-12 con url corregidas

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -13,7 +13,7 @@
       <div class="card-head3" style="width: 18rem;height:18rem;margin: 10px;border-width: 5px;border-color:rgba(180, 187, 181, 0.5);201, 207, 200;border-style: solid;border-radius: 32px 32px 32px 32px;">
         <img src="{{ asset('images/adidas1-1.jpg') }}" class="card-img-top" alt="Botines">
         <div class="card-body">
-          <a href="" class="btn btn-primary">{{ $category->name }}</a>
+          <a href="/categories/{{ $category->slug }}" class="btn btn-primary">{{ $category->name }}</a>
         </div>
       </div>
     @endforeach
