fd6591a584e92bd3fe40fc170b1d4d5fbdf816e3
alemaestre <51132131+alemaestre@users.noreply.github.com>
2019-12-20 20:35:59 -0300

    Update category-card.blade.php

--- resources/views/category-card.blade.php
+++ resources/views/category-card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-12 col-md-6 col-lg-4">
   <a href="{{ route('category', ['categorySlug' => $category->slug]) }}" class="card">
-    <img class="card-img-top" src="{{ $category->imageUrl }}" alt="Card image cap">
+    <img class="card-img-top" src="{{ asset('/images/backend_img/product/medium/'.$category->image) }}" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">{{ $category->name }}</h5>
     </div>
